ee5c60a2fc4dd8b46a7bf1dd05ad183807fccc35
Franklin <fshiao@purdue.edu>
2017-08-07 15:17:47 -0700

    Regular Expression Matching
    Implement regular expression matching with support for '.' and '*'.
    
    '.' Matches any single character.
    '*' Matches zero or more of the preceding element.
    
    The matching should cover the entire input string (not partial).
    
    The function prototype should be:
    bool isMatch(const char *s, const char *p)
    
    Some examples:
    isMatch("aa","a") ? false
    isMatch("aa","aa") ? true
    isMatch("aaa","aa") ? false
    isMatch("aa", "a*") ? true
    isMatch("aa", ".*") ? true
    isMatch("ab", ".*") ? true
    isMatch("aab", "c*a*b") ? true

---
+++ Regular Expression Matching.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        if(p == null || p.length() == 0)
+            return s == null || s.length() == 0;
+          
+        if (p.length() > 1 && '*' == p.charAt(1))
+            return (isMatch(s, p.substring(2)) || 
+                    s.length() != 0 && (s.charAt(0) == p.charAt(0) || '.' == p.charAt(0)) && isMatch(s.substring(1), p));
+        else
+            return s.length() != 0 && (s.charAt(0) == p.charAt(0) || '.' == p.charAt(0)) && isMatch(s.substring(1), p.substring(1));
+    }
+}
